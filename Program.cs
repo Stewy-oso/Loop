62a4733a6f8d18ba0b2d9aa7a5da68a3271df056
Stewy-oso <haplesstew5034@gmail.com>
2023-06-27 16:28:19 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Loop";
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+        for (int i = 10; i > 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+        ///Wait Before Closing.
+        Console.ReadKey();
+    }
+}
